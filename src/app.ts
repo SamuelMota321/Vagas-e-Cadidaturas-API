ad06b227047ebc0ce5859b075cd948be653badbb
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-08-19 17:43:50 -0300

    Alterações para efetuar o deploy

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import "express-async-errors";
 import express, { json } from "express";
 import helmet from "helmet";
+import cors from "cors";
 import { opportunityRouter } from "./routers/opportunity.routes";
 import { HandleErrors } from "./middlewares/handleErrors.middleware";
 import { userRouter } from "./routers/user.routes";
@@ -9,6 +10,7 @@ import { userRouter } from "./routers/user.routes";
 export const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(json());
 app.use("/opportunities", opportunityRouter);
 app.use("/users", userRouter);
